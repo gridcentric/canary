e554cbe5704e0ed8b58605d8c8ae196e6aa2155c
Rui Lin <rui@gridcentric.com>
2013-08-07 14:09:58 +0000

    More grizzly compatibility
    Change-Id: I3fb3783bd9fcd3f30429a7d4969fc588a6279fc8

--- canary/horizon/instances/panel.py
+++ canary/horizon/instances/panel.py
@@ -13,6 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from django.utils.translation import ugettext_lazy as _
+
 from canary.horizon import dashboard
 import horizon
 
